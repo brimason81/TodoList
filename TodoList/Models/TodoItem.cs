149f38ad880d5625470920bbcb1a2743685e2ced
Brian <brian.mason@worden.com>
2022-09-21 14:23:22 -0400

    Add Server-Side Validation to Model

--- TodoList/Models/TodoItem.cs
+++ TodoList/Models/TodoItem.cs
@@ -5,6 +5,7 @@ namespace TodoList.Models
     {
         //[Key]
         //public int Id { get; set; }
+        [Required]
         public string Item { get; set; }
         public bool IsDone { get; set; } = false;
     }
