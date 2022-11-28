17614cae32cac02d067fa0d776902bf09e62cabf
Brian <brian.mason@worden.com>
2022-11-28 14:35:22 -0500

    Create interface for TodoStore

---
+++ TodoList/store/ITodoStore.cs
@@ -0,0 +1,18 @@
+﻿using TodoList.Models;
+
+namespace TodoList.store
+{
+    public interface ITodoStore
+    {
+       
+        void AddOrUpdateTodoItem(TodoItem item);
+        void DeleteTodo(int id);
+        List<TodoItem> GetAllTodos();
+        int GetItemId();
+        string GetOrCreateDirectory();
+        string GetPathForItem(string item);
+        TodoItem GetTodo(string path);
+        TodoItem GetTodoById(int id);
+        void HandleIsDone(string item);
+    }
+}
